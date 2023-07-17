f15929d392b39e6b9ab77d229e513c4cdc8d8915
vitthal-korvan <korvanvitthal@gmail.com>
2023-07-17 17:44:41 +0530

    fevicon and script.js updated!

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+// document.querySelector('.message').textContent= '🎉 Correct Number'
+
+document.querySelector('.check').addEventListener('click', function(){
+      console.log(document.querySelector('.guess').value);
+})
