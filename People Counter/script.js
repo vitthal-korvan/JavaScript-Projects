7cd39567ba44d843c3e581cfe921647e129d7349
vitthal-korvan <korvanvitthal@gmail.com>
2024-06-10 12:39:56 +0530

    2 Projects Updated !

---
+++ People Counter/script.js
@@ -0,0 +1,21 @@
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    let countDash = count + " - ";
+    // 3. Render the variable in the saveEl using innerText
+    saveEl.textContent += countDash    
+    // 4. Reset the count to 0      
+    countEl.textContent = 0
+    count = 0
+}
+
+
