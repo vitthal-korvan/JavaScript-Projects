1300f0a652f697c87453635051795bc1537213ac
vitthal-korvan <korvanvitthal@gmail.com>
2024-06-11 11:17:28 +0530

    Shopping List Using FireBase Project added
    1st draft of firebase project

---
+++ Shopping-List-Firebase/index.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://realtime-database-df319-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    console.log(inputValue)
+})
