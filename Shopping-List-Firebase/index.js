b506ce9a94b954178954f6cb33335de20b663fe0
vitthal-korvan <korvanvitthal@gmail.com>
2024-06-11 11:29:26 +0530

    update 2 - getdatabase

--- Shopping-List-Firebase/index.js
+++ Shopping-List-Firebase/index.js
@@ -1,15 +1,20 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://realtime-database-df319-default-rtdb.europe-west1.firebasedatabase.app/"
-}
+  databaseURL:
+    "https://own-to-do-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-    
+     push(shoppingListInDB, inputValue);
     console.log(inputValue)
 })
